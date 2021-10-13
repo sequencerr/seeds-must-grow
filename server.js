e197ecd2213793699688be738b06edc075ebfb18
thebitler <yurchyknikita@gmail.com>
2021-10-14 00:48:25 +0300

    Fix try to fix server

--- server.js
+++ server.js
@@ -2,41 +2,40 @@ const express = require('express');
 const app = express();
 const expressStaticGzip = require('express-static-gzip');
 const { resolve } = require('path');
+// const engines = require('consolidate');
 
 const { NODE_ENV, PORT } = process.env;
 const port = PORT || 5000;
 
-if (NODE_ENV != 'production') return;
+// if (NODE_ENV != 'production') return;
 
-// https://stackoverflow.com/questions/55353671/why-do-i-get-a-df-worker-min-js-was-loaded-even-though-its-mime-type-text-ht
-// app.use('*.js', (_req, res, next) => {
-// 	res.set('Content-Type', 'text/javascript');
-// 	next();
-// });
-app.use(express.static(resolve(__dirname, 'build')));
-// "Heroku Cannot GET /" https://stackoverflow.com/a/59203812
-app.get('*', (_req, res) => {
-	res.sendFile(resolve(__dirname, 'build', 'index.html'));
-});
-
+app.use(express.static(resolve('build')));
+// "Cannot GET /" https://stackoverflow.com/a/59203812
 app.set('view options', { layout: false });
-app.get('/', (_req, res) => {
-	res.render('/build/index.html');
-});
 
+// "Cannot find module 'html'" https://stackoverflow.com/questions/16111386/error-cannot-find-module-html/62611449
+// app.engine('html', engines.mustache);
+// app.set('view engine', 'html');
+
+app.get('/', (_req, res) => {
+	res.render(resolve('build', 'index.html'));
+});
+// app.get('*', (_req, res) => {
+// 	res.sendFile(resolve('build', 'index.html'));
+// });
 // https://github.com/tkoenig89/express-static-gzip#behavior-warning
 // https://tech.groww.in/enable-brotli-compression-in-webpack-with-fallback-to-gzip-397a57cf9fc6
-// app.use(
-// 	'/build',
-// 	expressStaticGzip('/build', {
-// 		index: false,
-// 		enableBrotli: true,
-// 		orderPreference: ['br', 'gz'],
-// 		serveStatic: {
-// 			maxAge: 31536000 // 1 year
-// 		}
-// 	})
-// );
+app.use(
+	'/build',
+	expressStaticGzip('/build', {
+		index: false,
+		enableBrotli: true,
+		orderPreference: ['br', 'gz'],
+		serveStatic: {
+			maxAge: 31536000 // 1 year
+		}
+	})
+);
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
